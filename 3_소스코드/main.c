e69735766d456d3c000250f4d66ee0f2815dfca7
gwanggyos240122-create <gwanggyo.s240122@ggh.goe.go.kr>
2025-11-19 05:30:18 +0000

    20211박태민

--- 3_소스코드/main.c
+++ 3_소스코드/main.c
@@ -1,36 +1,36 @@
 #include <stdio.h>
 
-/*
-    1. [설계]에서 정의한 변수, 배열, (필요하면) 구조체를 여기에 선언하세요.
-    
-    예시 - 배열 사용:
-    char names[100][20];
-    int scores[100];
-    int count = 0;
-    
-    예시 - 구조체 사용 (선택):
-    struct Student {
-        char name[20];
-        int score;
-    };
-    struct Student students[100];
-*/
-
-
-/*
-    2. [알고리즘]에서 설계한 핵심 기능 함수들을 여기에 선언하세요.
-*/
-
+typedef struct {
+    int g, c, n;
+    char name[30];
+} Stu;
 
 int main() {
-    
-    printf("--- C언어 미니 프로젝트 시작! ---\n");
+    Stu s[100];
+    int i, j, t, cnt;
 
-    /*
-        3. [알고리즘]에서 설계한 main 함수의 흐름을
-           여기에 C언어로 자유롭게 구현하세요.
-    */
-    
-    
+    scanf("%d", &cnt); // 학생 수
+
+    for(i = 0; i < cnt; i++)
+        scanf("%d %d %d %s", &s[i].g, &s[i].c, &s[i].n, s[i].name);
+
+    // 정렬
+    for(i = 0; i < cnt - 1; i++) {
+        for(j = i + 1; j < cnt; j++) {
+            if(s[i].g > s[j].g ||
+              (s[i].g == s[j].g && s[i].c > s[j].c) ||
+              (s[i].g == s[j].g && s[i].c == s[j].c && s[i].n > s[j].n)) {
+
+                Stu temp = s[i];
+                s[i] = s[j];
+                s[j] = temp;
+            }
+        }
+    }
+
+    // 출력
+    for(i = 0; i < cnt; i++)
+        printf("%d %d %d %s\n", s[i].g, s[i].c, s[i].n, s[i].name);
+
     return 0;
 }
